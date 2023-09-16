67a5ed160129e96f23fb3e967768650fe27408f8
Oleksandr Smuhliakov <oleksandr.smuhliakov@accurids.com>
2023-09-16 22:36:53 +0200

    Виконані завдання та додані додаткові тести

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,6 +8,15 @@ fun main() {
     var a:Int = 2
     var res = 3.7
     a = (a * res).toInt()
+    println("Enter the x:")
+    val x = readln().toDouble()
+    println("Enter the a:")
+    val a1 = readln().toDouble()
+    println("Enter the b:")
+    val b = readln().toDouble()
+
+    println("Answer(S): "+"%6f".format(var5calcS(x,a1,b))+
+            "\n"+ "Answer(Q): "+ "%6f".format(var5calcQ(x,a1,b)))
 }
 
 /* Пример написания функции, состоящей из одного выражения
@@ -15,42 +24,8 @@ fun main() {
  */
 fun f(x:Double) = sin(x) + cos(x)
 
-fun var1calcR(a: Double, b: Double, x: Double) : Double = x*x*(x+1)/b
 
-fun var1calcS(a: Double, b: Double, x: Double) : Double = TODO()
+//Смугляков
+fun var5calcS(x:Double, a:Double, b: Double) : Double  =  a/sqrt(x+b) + x.pow(3) * (tan((x+b).pow(2)).pow(2))
 
-fun var2calcF(m: Int, c: Int, t: Double) : Double = TODO()
-
-fun var2calcZ(m: Int, c: Int, t: Double, b: Double) : Double = TODO()
-
-fun var3calcY(a: Double, b: Double, x: Double) : Double = TODO()
-
-fun var3calcD(a: Double, b: Double, x: Double) : Double = TODO()
-
-fun var4calcS(x: Double) : Double = TODO()
-
-fun var4calcF(x: Double, y: Double) : Double = TODO()
-
-fun var5calcS(x:Double, a:Double, b: Double) : Double = TODO()
-
-fun var5calcQ(x: Double, a:Double, b:Double) : Double = TODO()
-
-fun var6calcY(a: Double, b:Double, t:Double) : Double = TODO()
-
-fun var6calcS(a: Double, b:Double, t:Double) : Double = TODO()
-
-fun var7calcY(a:Double, b: Double, x:Double) : Double = TODO()
-
-fun var7calcZ(a:Double, b: Double, x:Double) : Double = TODO()
-
-fun var8calcA(x: Double, y: Double, z: Double) : Double = TODO()
-
-fun var8calcB(x: Double, y: Double, z: Double) : Double = TODO()
-
-fun var9calcW(x: Double, a: Double, b: Double) : Double = TODO()
-
-fun var9calcY(x: Double, a: Double, b: Double) : Double = TODO()
-
-fun var10calcC(x: Double, y: Double, z: Double) : Double = TODO()
-
-fun var10calcF(x: Double, y:Double, z:Double) : Double = TODO()
+fun var5calcQ(x: Double, a:Double, b:Double) : Double = (b*x.pow(2) - a )/ (E.pow(a*x) - 1)
